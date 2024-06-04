845540111294205e15b915a036bb2a0390732da4
DaianePorcena <daiporcenna@gmail.com>
2024-06-04 12:11:24 -0300

    Atualiza título da aplicação e adiciona tabela com desafio da aplicação

--- introducao/hello-world/src/App.js
+++ introducao/hello-world/src/App.js
@@ -3,7 +3,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World React</h1>
+      <h1>Hello World React!</h1>
+      <h2>Emmet funcionou</h2>
+
+      <h3>Desafio!!!</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Instruções</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1. Entrar no arquivo App.js e adicionar mais algum elemento HTML</td>
+            <td>[x]</td>
+          </tr>
+          <tr>
+            <td>2. Crie uma regra de estilos que altere a cor do elemento</td>
+            <td>[x]</td>
+          </tr>
+          <tr>
+            <td>3. Vá até o index.html e altere o título da aplicação na meta tag</td>
+            <td>[x]</td>
+          </tr>
+        </tbody>
+        
+      </table>
     </div>
   );
 }
