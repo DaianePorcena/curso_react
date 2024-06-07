365d7bd5ac4402dbdfa1acbf4097dad2415c5974
DaianePorcena <daiporcenna@gmail.com>
2024-06-07 16:01:44 -0300

    Projeto Secret Word

---
+++ 06_secret_words/secretword/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordlist = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Volante"],
+  fruta: ["Maçã", "Banana", "Uva", "Pera", "Melancia"],
+  corpo: ["Cerebro", "Braço", "Perna", "Olhos", "Pescoço"],
+  computador: ["Teclado", "Mouse", "Monitor", "CPU", "Placa de vídeo"],
+  programacao: ["Javascript", "Linguagem", "Framework", "React", "Java"],
+  alimento: ["Arroz", "Feijão", "Carne", "Salada", "Macarrão"],
+};
