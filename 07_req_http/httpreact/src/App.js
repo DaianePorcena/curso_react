f41d558799a6777a12c4b3b483bf36cd744b0be5
DaianePorcena <daiporcenna@gmail.com>
2024-06-10 13:56:22 -0300

    feat: Adiciona formulário para criação de produtos e hooks para controle da api

--- 07_req_http/httpreact/src/App.js
+++ 07_req_http/httpreact/src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+// custom hook
+import { useFetch } from "./hooks/useFetch";
+
+const url = "http://localhost:3000/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const { data: items, httpConfig } = useFetch(url);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  // 1 - resgatando dados
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
+
+  //     const data = await res.json();
+
+  //     setProducts(data);
+  //   }
+
+  //   fetchData();
+  // }, []);
+
+  // 2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(product),
+    // });
+
+    // 3 - carregamento dinâmico
+    //
+
+    // 5 - refatorando post
+    httpConfig(product, "POST");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+            </li>
+          ))}
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="text"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value={"Criar"} />
+        </form>
+      </div>
     </div>
   );
 }
