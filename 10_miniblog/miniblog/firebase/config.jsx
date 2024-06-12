fb62dfd1274914dcee0b3739eb019050b788ff16
DaianePorcena <daiporcenna@gmail.com>
2024-06-12 10:27:08 -0300

    configuração do firebase

---
+++ 10_miniblog/miniblog/firebase/config.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuILXhHSTXkyQyXRU4qgNbKwkeTbDHetM",
+  authDomain: "miniblog-react-59cce.firebaseapp.com",
+  projectId: "miniblog-react-59cce",
+  storageBucket: "miniblog-react-59cce.appspot.com",
+  messagingSenderId: "451636826237",
+  appId: "1:451636826237:web:a6259909c13bc05d6fa114",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
