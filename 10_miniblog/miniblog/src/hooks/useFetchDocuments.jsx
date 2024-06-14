ca8ce683b44db3efb5fd659c3a4e81a8ac1de5c7
DaianePorcena <daiporcenna@gmail.com>
2024-06-14 12:35:57 -0300

    Ajuste no hook de resgate de dados

--- 10_miniblog/miniblog/src/hooks/useFetchDocuments.jsx
+++ 10_miniblog/miniblog/src/hooks/useFetchDocuments.jsx
@@ -6,24 +6,23 @@ import {
   orderBy,
   onSnapshot,
   where,
-} from "firebase/firestore/lite";
-import { QuerySnapshot } from "firebase/firestore";
+} from "firebase/firestore";
 
 export const useFetchDocuments = (docCollection, search = null, uid = null) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(null);
 
-  // memory leak
+  // deal with memory leak
   const [cancelled, setCancelled] = useState(false);
 
   useEffect(() => {
     async function loadData() {
-      if (cancelled) return;
+      if (cancelled) {
+        return;
+      }
 
-      setLoading(true);
-
-      const collectionRef = collection(db, docCollection);
+      const collectionRef = await collection(db, docCollection);
 
       try {
         let q;
@@ -38,12 +37,10 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
             }))
           );
         });
-
         setLoading(false);
       } catch (error) {
         console.log(error);
         setError(error.message);
-
         setLoading(false);
       }
     }
